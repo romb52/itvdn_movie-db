037cdad03faecb092f508748c19ce7ea0d51b25b
romb52 <rom.bom1980@gmail.com>
2025-01-24 12:37:21 +0200

    add tmdb token

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -5,6 +5,21 @@ import { MovieCard } from "./MovieCard";
 
 import styles from  "./Movies.module.scss";
 
+function getNowPlaying(){
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYzI1MWQ0ZGE5MTA0NzU3OTBiYmYzY2FlZTdjZmMwNSIsIm5iZiI6MTczNzcxNDI1OS4zMTgsInN1YiI6IjY3OTM2YTUzMjVkMjk4MGZiMDIzOTIyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NOFpUjXaI59thC0dW8QKbgNm-l3gezxntV2UjZzdWAk'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', options)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+}
+
 
 interface MoviesProps {
     movies: Movie[]
